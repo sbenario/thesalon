cc9a2f711361a328d4092d572ceb8a236353372c
Steven Benario <sbenario@gmail.com>
2013-04-01 16:36:34 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thesalon::Application.config.secret_token = 'f5fc8fde8a9a0c087e336e25d72205e5122d20af6761add13a52118dc7ad99ca3b6021243c33ff0797b99cf741a00901e9fe90e22633a3bf22b6e40899088236'
